2fcf09a854e7733342c8220e8f6b731633afcbfc
Chill <baophi852@gmail.com>
2026-02-23 21:49:11 +0700

    commit some

--- backend/Src/PIED_LMS.Presentation/APIs/QuizletEndpoints.cs
+++ backend/Src/PIED_LMS.Presentation/APIs/QuizletEndpoints.cs
@@ -19,24 +19,27 @@ public class QuizletEndpoints : ICarterModule
         // POST /api/quizlets
         group.MapPost("", CreateQuizlet)
             .WithName("CreateQuizlet")
-            .DisableAntiforgery();
+            .DisableAntiforgery()
+            .RequireAuthorization(policy => policy.RequireRole("Admin", "Mentor", "Teacher")); 
 
         // GET /api/quizlets  (Admin, Mentor, Lecturer — all quizlets summary)
         group.MapGet("", GetAllQuizlets)
             .WithName("GetAllQuizlets")
-            .RequireAuthorization(policy => policy.RequireRole("Admin", "Mentor", "Lecturer"));
+            .RequireAuthorization(policy => policy.RequireRole("Admin", "Mentor", "Teacher"));
 
         group.MapGet("/{id:int}", GetQuizletById)
             .WithName("GetQuizletById")
-            .RequireAuthorization(policy => policy.RequireRole("Admin", "Mentor", "Lecturer"));
+            .RequireAuthorization(policy => policy.RequireRole("Admin", "Mentor", "Teacher"));
 
         // DELETE /api/quizlets/{id}
         group.MapDelete("/{id}", DeleteQuizlet)
-            .WithName("DeleteQuizlet");
+            .WithName("DeleteQuizlet")
+            .RequireAuthorization(policy => policy.RequireRole("Admin", "Mentor", "Teacher")); ;
 
         // PUT /api/quizlets/{id}
         group.MapPut("/{id}", UpdateQuizlet)
-            .WithName("UpdateQuizlet");
+            .WithName("UpdateQuizlet")
+            .RequireAuthorization(policy => policy.RequireRole("Admin", "Mentor", "Teacher")); ;
 
         // GET /api/students/quizlets  (Student — published only, summary)
         app.MapGet("/api/students/quizlets", GetStudentQuizlets)
